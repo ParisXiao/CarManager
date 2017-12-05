8170928e61e0adc547e70c56c19b4d023275e73f
ParisXiao <1451183076@qq.com>
2017-12-05 16:36:27 +0800

    蓝牙热敏打印机

---
+++ app/src/main/java/com/pda/carmanager/config/BlueConfig.java
@@ -0,0 +1,9 @@
+package com.pda.carmanager.config;
+
+/**
+ * Created by Admin on 2017/12/5.
+ */
+
+public class BlueConfig {
+    public final static  String UUID="00001101-0000-1000-8000-00805F9B34FB";
+}
